1745b8337e633bd73bf2d6a04e9519c6f2903ec9
Francisco Schwertner <rockchico@gmail.com>
2018-12-10 09:50:44 -0200

    incio tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toggle: true,
+      list: ['a', 'b', 'c'],
+    };
+  }
+
+  onToggleList = () => {
+    this.setState(prevState => ({ toggle: !prevState.toggle }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Toggle
+          toggle={this.state.toggle}
+          onToggleList={this.onToggleList}
+        />
+        {this.state.toggle && <List list={this.state.list} />}
       </div>
     );
   }
 }
+
+const Toggle = ({ toggle, onToggleList }) => (
+  <button type="button" onClick={onToggleList}>
+    {toggle ? 'Hide' : 'Show'}
+  </button>
+);
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item }) => <li>{item}</li>;
 
 export default App;
